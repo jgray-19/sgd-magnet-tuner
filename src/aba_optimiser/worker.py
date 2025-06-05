f5b039e2506d05c486b283e42c7d296b23015a18
Joshua Gray <61458311+jgray-19@users.noreply.github.com>
2025-06-05 15:17:16 +0200

    Update worker to be able to take different x y lengths

--- src/aba_optimiser/worker.py
+++ src/aba_optimiser/worker.py
@@ -37,9 +37,11 @@ class Worker(Process):
 
         self.x_indices = x_indices
         self.y_indices = y_indices
-        self.num_particles = len(x_indices)  # Assuming x_indices and y_indices are of the same length
-        assert len(y_indices) == self.num_particles, "x_indices and y_indices must have the same length."
-        
+        self.num_particles = {
+            X_BPM_START: len(x_indices),
+            Y_BPM_START: len(y_indices),
+        }
+
         comparison_data = comparison_data.reset_index().set_index('turn')
         self.comparison_data = comparison_data
         self.start_data_dict = start_data_dict
@@ -55,17 +57,18 @@ class Worker(Process):
         and difference matrices.
         """
         return f"""
-dx_dk    = table.new(num_particles, 0)
-dy_dk    = table.new(num_particles, 0)
-for i=1,num_particles do
+local dx_dk = table.new(num_particles[starting_bpm], 0)
+local dy_dk = table.new(num_particles[starting_bpm], 0)
+for i=1,num_particles[starting_bpm] do
     dx_dk[i] = matrix(#knob_names, win_lengths[starting_bpm])
     dy_dk[i] = matrix(#knob_names, win_lengths[starting_bpm])
 end
 local sim_trk = track{{sequence = MADX.{SEQ_NAME}, X0 = da_x0_c[starting_bpm], nturn = 1, savemap=true, range=range}}
 assert(sim_trk.lost == 0, "Tracking failed for initial conditions")
-for i = 1, num_particles do
+for i = 1, num_particles[starting_bpm] do
     for j = 1, win_lengths[starting_bpm] do
-        local map = sim_trk.__map[i + (j - 1) * num_particles]
+        local map = sim_trk.__map[i + (j - 1) * num_particles[starting_bpm]]
+        assert(sim_trk.id[i + (j - 1) * num_particles[starting_bpm]] == i, "Mismatch between track ID and index")
         for k, param in ipairs(knob_names) do
             dx_dk[i]:set(k, j, map.x:get(knob_monomials[param]))
             dy_dk[i]:set(k, j, map.y:get(knob_monomials[param]))
@@ -94,15 +97,19 @@ local monomial, matrix in MAD
 local num_knobs = #knob_names
 local ncoord = #coord_names
 local Htot = matrix(num_knobs, num_knobs):zeros()
+local vector in MAD
+W_x = vector(win_lengths[starting_bpm]):fill(var_x):diag()
+W_y = vector(win_lengths[starting_bpm]):fill(var_y):diag()
 
 local trk = track{{sequence=MADX.{SEQ_NAME}, X0=da_x0_c[starting_bpm], nturn=1, savemap=true, range=range}}
-for idx = 1, num_particles do
+assert(trk.lost == 0, "Tracking failed for initial conditions")
+for idx = 1, num_particles[starting_bpm] do
     -- build jacobian matrices jx, jy -----------------------------
-    local jx = matrix(num_knobs, nbpms):zeros()
-    local jy = matrix(num_knobs, nbpms):zeros()
-    for b = 1, nbpms do
-        local map = trk.__map[idx + (b - 1) * num_particles]
-        assert(trk.id[idx + (b - 1) * num_particles] == idx, "Mismatch between track ID and index")
+    local jx = matrix(num_knobs, win_lengths[starting_bpm]):zeros()
+    local jy = matrix(num_knobs, win_lengths[starting_bpm]):zeros()
+    for b = 1, win_lengths[starting_bpm] do
+        local map = trk.__map[idx + (b - 1) * num_particles[starting_bpm]]
+        assert(trk.id[idx + (b - 1) * num_particles[starting_bpm]] == idx, "Mismatch between track ID and index")
         for k, param in ipairs(knob_names) do
             local mono = knob_monomials[param]
             jx:set(k,b, map.x:get(mono))
@@ -115,7 +122,7 @@ for idx = 1, num_particles do
     Htot = Htot + (Gx + Gy)
 end
 if {str(diagonal_only).lower()} then
-    py:send(Htot:diag() / num_particles)
+    py:send(Htot:diag() / num_particles[starting_bpm])
 else
     py:send(Htot)
 end
@@ -135,16 +142,16 @@ end
         Sets the initial conditions for each track in MAD-NG.
         """
         mad.send("da_x0_c = {}")
-        for key, value in selected_init.items():
+        for start_bpm, initial_conditions in selected_init.items():
             mad.send(f"""
-    da_x0_c['{key}'] = table.new(num_particles, 0)
+    da_x0_c['{start_bpm}'] = table.new(num_particles['{start_bpm}'], 0)
     init_coords = py:recv()
-    for i=1,num_particles do 
-        da_x0_c['{key}'][i] = da_x0_base:copy() 
-        da_x0_c['{key}'][i]:set0(init_coords[i])
+    for i=1,num_particles['{start_bpm}'] do 
+        da_x0_c['{start_bpm}'][i] = da_x0_base:copy() 
+        da_x0_c['{start_bpm}'][i]:set0(init_coords[i])
     end
             """)
-            mad.send(value)
+            mad.send(initial_conditions)
 
     def run(self) -> None:
         # Load initial conditions
@@ -157,25 +164,30 @@ end
         y_comparisons: dict[str, np.ndarray] = {}
         x_masks: dict[str, np.ndarray] = {}
         y_masks: dict[str, np.ndarray] = {}
+        x_weights: dict[str, np.ndarray] = {}
+        y_weights: dict[str, np.ndarray] = {}
         for start_bpm, start_df in self.start_data_dict.items():
             init_coords[start_bpm] = []
+            win_slice = self.window_slices[start_bpm]
+            num_parts = self.num_particles[start_bpm]
+            win_length = win_slice.stop - win_slice.start
+
+            x_masks[start_bpm] = np.ones((num_parts, win_length), dtype=bool)
+            y_masks[start_bpm] = np.ones((num_parts, win_length), dtype=bool)
+
             if start_bpm == X_BPM_START:
-                # x_masks[i][:, 1::2] = False  # x BPMs are even indexed
-                # y_masks[i][:, ::2] = False  # y BPMs are odd indexed
+                # x_masks[start_bpm][:, 1::2] = False  # x BPMs are even indexed
+                # y_masks[start_bpm][:, ::2] = False  # y BPMs are odd indexed
                 indices = self.x_indices
             else:
-                # x_masks[i][:, ::2] = False  # x BPMs are odd indexed
-                # y_masks[i][:, 1::2] = False  # y BPMs are even indexed
+                # x_masks[start_bpm][:, ::2] = False  # x BPMs are odd indexed
+                # y_masks[start_bpm][:, 1::2] = False  # y BPMs are even indexed
                 indices = self.y_indices
-                   
-            win_slice = self.window_slices[start_bpm]
-            win_length = win_slice.stop - win_slice.start
-            x_comparisons[start_bpm] = np.empty((self.num_particles, win_length))
-            y_comparisons[start_bpm] = np.empty((self.num_particles, win_length))
 
-            x_masks[start_bpm] = np.ones((self.num_particles, win_length), dtype=bool)
-            y_masks[start_bpm] = np.ones((self.num_particles, win_length), dtype=bool)
-
+            x_comparisons[start_bpm] = np.empty((num_parts, win_length))
+            y_comparisons[start_bpm] = np.empty((num_parts, win_length))
+            x_weights[start_bpm] = np.empty((num_parts, win_length))
+            y_weights[start_bpm] = np.empty((num_parts, win_length))
             win_lengths[start_bpm] = win_length
 
             turn_start_df = start_df.reset_index().set_index('turn')
@@ -189,7 +201,9 @@ end
                     raise ValueError(f"No data available for turn {turn} in window {start_bpm}")
                 x_comparisons[start_bpm][j, :] = filtered["x"].to_numpy()
                 y_comparisons[start_bpm][j, :] = filtered["y"].to_numpy()
-
+                x_weights[start_bpm][j, :] = filtered["weight_x"].to_numpy()
+                y_weights[start_bpm][j, :] = filtered["weight_y"].to_numpy()
+            
         del self.start_data_dict
         del self.comparison_data
 
@@ -198,7 +212,10 @@ end
             SEQUENCE_FILE, BPM_RANGE, #discard_mad_output=False
         )
         mad = mad_iface.mad
-        mad_iface.mad["num_particles"] = self.num_particles
+        # mad_iface.mad["num_particles"] = self.num_particles
+        mad.send('num_particles = {}')
+        for start_bpm, num_parts in self.num_particles.items():
+            mad.send(f"num_particles['{start_bpm}'] = {num_parts}")
 
         mad.send("win_lengths = {}")
         for start_bpm, win_length in win_lengths.items():
@@ -222,15 +239,7 @@ end
         self.send_initial_conditions(mad, init_coords)
         # Main loop: receive messages, run tracking, and send full per-BPM data
         track_code = self.build_batch_tracking_code()
-        hessian_var_x, hessian_var_y = self.conn.recv()
-        mad_iface.mad.send("""
-local vector in MAD
-W_x = vector(py:recv()):diag()
-W_y = vector(py:recv()):diag()
-""")
-        mad_iface.mad.send((1/hessian_var_x).tolist())
-        mad_iface.mad.send((1/hessian_var_y).tolist())
-        
+        hessian_var_x, hessian_var_y = self.conn.recv()        
         knob_updates = self.conn.recv()  # shape (n_knobs,)
         while knob_updates is not None:
             grad = np.zeros(len(knob_updates))
@@ -252,10 +261,10 @@ W_y = vector(py:recv()):diag()
                 dx_stack = np.stack(mad.recv(), axis=0)
                 dy_stack = np.stack(mad.recv(), axis=0)
                 
-                x_res = mad.recv().reshape(self.num_particles, win_lengths[start_bpm], order='F')
-                y_res = mad.recv().reshape(self.num_particles, win_lengths[start_bpm], order='F')
-                x_diffs = x_res - x_comparisons[start_bpm]
-                y_diffs = y_res - y_comparisons[start_bpm]
+                x_res = mad.recv().reshape(self.num_particles[start_bpm], win_lengths[start_bpm], order='F')
+                y_res = mad.recv().reshape(self.num_particles[start_bpm], win_lengths[start_bpm], order='F')
+                x_diffs = (x_res - x_comparisons[start_bpm]) * x_masks[start_bpm] * x_weights[start_bpm]
+                y_diffs = (y_res - y_comparisons[start_bpm]) * y_masks[start_bpm] * y_weights[start_bpm]
 
                 # Build gradient and loss
                 grad += (
@@ -273,6 +282,14 @@ W_y = vector(py:recv()):diag()
             # 6) Receive the knob updates and new slice
             knob_updates = self.conn.recv()
 
+
+        mad_iface.mad.send("""
+var_x = py:recv()
+var_y = py:recv()
+""")
+        mad_iface.mad.send((1/hessian_var_x).mean())
+        mad_iface.mad.send((1/hessian_var_y).mean())
+
         # Update the knobs to order 2.
         self.create_base_damap(mad, knob_order=2)
         self.send_initial_conditions(mad, init_coords)
@@ -281,8 +298,12 @@ for i,param in ipairs(knob_names) do
     MADX.{SEQ_NAME}[param] = MADX.{SEQ_NAME}[param]:get0() + da_x0_base[param]
 end
 """)
-        mad.send(f'range = "{BPM_RANGE}"') # Reset the range to the full BPM range
-        mad.send(f'starting_bpm = "{BPM_RANGE.split("/")[0]}"')
-        mad_iface.mad.send(self.estimate_hessian())
-        H_part = mad_iface.mad.recv()
+        H_parts = {}
+        for start_bpm, end_bpm in WINDOWS:
+            mad.send(f'range = "{start_bpm}/{end_bpm}"')
+            mad.send(f'starting_bpm = "{start_bpm}"')
+            mad_iface.mad.send(self.estimate_hessian())
+            H_parts[start_bpm] = mad_iface.mad.recv()
+
+        H_part = sum(H_parts.values())
         self.conn.send(H_part)  # shape (n_knobs, n_knobs)
