41f1547f41a8e6596fb6be11fa132baef7abb008
Joshua Gray <61458311+jgray-19@users.noreply.github.com>
2026-02-03 00:09:27 +0100

    Remove TrackingMadInterface and related tracking utilities

--- src/aba_optimiser/mad/__init__.py
+++ src/aba_optimiser/mad/__init__.py
@@ -4,17 +4,12 @@ MAD-NG interface modules for accelerator simulation.
 This package provides different interfaces for working with MAD-NG:
 - BaseMadInterface: Core functionality without automatic setup
 - optimisationMadInterface: For accelerator optimisation workflows
-- TrackingMadInterface: Lightweight interface for tracking simulations
 """
 
 from .base_mad_interface import BaseMadInterface
 from .optimising_mad_interface import OptimisationMadInterface
-from .tracking_interface import (
-    TrackingMadInterface,
-)
 
 __all__ = [
     "BaseMadInterface",
     "OptimisationMadInterface",
-    "TrackingMadInterface",
 ]
